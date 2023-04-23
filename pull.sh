7d7098acfdb70b8f8b61c12c3312ac0c0c3f3488
gotwarlost <krishnan@the-krishnans.com>
2023-04-23 12:40:07 -0700

    misc

---
+++ pull.sh
@@ -0,0 +1,2 @@
+cp ~/"Ableton/User Library/Presets/MIDI Effects/Max MIDI Effect/rando-rondo.amxd" ./m4l-devices/rando-rondo/
+cp ~/"Ableton/User Library/Presets/Audio Effects/Max Audio Effect/xfader.amxd" ./m4l-devices/xfader/
